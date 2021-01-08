70e43ce0ec41f822e6dfa706205061618890652b
Jovani Pink <jovanirosado@hotmail.com>
2021-01-07 20:51:14 -0500

    aernas and data prep for analysis

---
+++ gathering/utils/merge.py
@@ -0,0 +1,124 @@
+nba_stats_columns = [
+    "PLAYER_ID",
+    "PLAYER_NAME",  # "Player"
+    "TEAM_ID",
+    "TEAM_ABBREVIATION",
+    "AGE",  # "Age"
+    "GP",  # "G"
+    "W",
+    "L",
+    "W_PCT",
+    "MIN",  # "MP"
+    "FGM",  # "FG"
+    "FGA",  # "FGA"
+    "FG_PCT",  # "FG%"
+    "FG3M",  # "3P"
+    "FG3A",  # "3PA"
+    "FG3_PCT",  # "3P%"
+    "FTM",  # "FT"
+    "FTA",  # "FTA"
+    "FT_PCT",  # "FT%"
+    "OREB",  # "ORB"
+    "DREB",  # "DRB"
+    "REB",  # "TRB"
+    "AST",  # "AST"
+    "TOV",  # "TOV"
+    "STL",  # "STL"
+    "BLK",  # "BLK"
+    "BLKA",
+    "PF",  # "PF"
+    "PFD",
+    "PTS",  # "PTS"
+    "PLUS_MINUS",
+    "NBA_FANTASY_PTS",
+    "DD2",
+    "TD3",
+    "GP_RANK",
+    "W_RANK",
+    "L_RANK",
+    "W_PCT_RANK",
+    "MIN_RANK",
+    "FGM_RANK",
+    "FGA_RANK",
+    "FG_PCT_RANK",
+    "FG3M_RANK",
+    "FG3A_RANK",
+    "FG3_PCT_RANK",
+    "FTM_RANK",
+    "FTA_RANK",
+    "FT_PCT_RANK",
+    "OREB_RANK",
+    "DREB_RANK",
+    "REB_RANK",
+    "AST_RANK",
+    "TOV_RANK",
+    "STL_RANK",
+    "BLK_RANK",
+    "BLKA_RANK",
+    "PF_RANK",
+    "PFD_RANK",
+    "PTS_RANK",
+    "PLUS_MINUS_RANK",
+    "NBA_FANTASY_PTS_RANK",
+    "DD2_RANK",
+    "TD3_RANK",
+    "CFID",
+    "CFPARAMS",
+]
+
+basketball_reference_columns = [
+    "Player",
+    "Pos",
+    "Age",
+    "Tm",
+    "G",
+    "GS",
+    "MP",
+    "FG",
+    "FGA",
+    "FG%",
+    "3P",
+    "3PA",
+    "3P%",
+    "2P",
+    "2PA",
+    "2P%",
+    "eFG%",
+    "FT",
+    "FTA",
+    "FT%",
+    "ORB",
+    "DRB",
+    "TRB",
+    "AST",
+    "STL",
+    "BLK",
+    "TOV",
+    "PF",
+    "PTS",
+]
+
+drop_these_columns = [
+    "Player",
+    "Age",
+    "G",
+    "MP",
+    "FG",
+    "FGA",
+    "FG%",
+    "3P",
+    "3PA",
+    "3P%",
+    "FT",
+    "FTA",
+    "FT%",
+    "ORB",
+    "DRB",
+    "TRB",
+    "AST",
+    "TOV",
+    "STL",
+    "BLK",
+    "PF",
+    "PTS",
+]
